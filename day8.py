6da783a8569cfa336aa3c94f915d6ac54deb8daf
logical1862 <100737462+logical1862@users.noreply.github.com>
2022-12-17 14:18:07 -0600

    day 8 part two complete

--- day8.py
+++ day8.py
@@ -1,11 +1,8 @@
 """Find a good tree"""
 
-def count_tree(current_count: int) ->int:
-    return current_count + 1
-    
-
 
 def part_one(trees: list) -> int:
+    """Returns number of visible trees"""
     num_columns = len(trees[0])
     num_rows = 0
 
@@ -55,14 +52,83 @@ def part_one(trees: list) -> int:
                 blocked_trees += 1
     return (num_columns * num_rows) - blocked_trees
 
+def part_two(trees: list) -> int:
+    """returns highest scenic score from tree map"""
+    # gonna need some counters
+    high_score = 0
+    count_up = 0
+    count_down = 0
+    count_left = 0
+    count_right = 0
+
+
+    for row, row_sight in enumerate(trees):
+        # edges will have a score of zero so well skip them
+        if row in (0,len(trees) - 1):
+            continue
+
+        # for each tree
+        for count, tree in enumerate(row_sight):
+            tree = int(tree)
+            # for each dir, increment each counter until blocked or edge
+            # calc score, store it,  reset counters
+
+            # skip edges
+            last_tree = len(row_sight) - 1
+            if count in (0, last_tree):
+                continue
+
+            # unpack rows first
+            beg_row = [*row_sight[:count]]
+            end_row = [*row_sight[count+1:]]
+
+            # left
+            for next_tree in range(count - 1, -1, -1):
+                count_left += 1
+
+                if int(beg_row[next_tree]) >= tree:
+                    break
+
+            # right
+            for next_tree in end_row:
+                count_right += 1
+                if int(next_tree) >= tree:
+                    break
+
+            # columns going down
+            for i in range(row + 1, len(trees)):
+                count_down += 1
+                if int(trees[i][count]) >= tree:
+                    break
+
+            # going up
+            for j in range(row - 1, -1, -1):
+                count_up += 1
+                if int(trees[j][count]) >= tree:
+                   break
+
+
+            # now calc the scenic score
+            score = count_left * count_right * count_up * count_down
+            # reset counters
+            count_up = 0
+            count_down = 0
+            count_left = 0
+            count_right = 0
+
+            if score > high_score:
+                high_score = score
+    return high_score
+
 if __name__ == "__main__":
     path = 'AdventOfCode2022\\day8_input.txt'
     test_path = 'AdventOfCode2022\\day8testinput.txt'
     test_two_path = 'AdventOfCode2022\\day8test2.txt'
 
-    with open(test_path, 'rt', encoding='UTF-8') as tree_map_input:
+    with open(path, 'rt', encoding='UTF-8') as tree_map_input:
         tree_map = [x.strip('\n') for x in tree_map_input.readlines()]
 
 
     print(part_one(tree_map))
+    print(part_two(tree_map))
     
